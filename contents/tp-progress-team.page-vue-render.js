8903609b24b981dfb9efaa174205bcf0fc9b67b3
damithc <1673303+damithc@users.noreply.github.com>
2024-10-22 11:59:47 +0800

    Site Update. [skip ci]

--- contents/tp-progress-team.page-vue-render.js
+++ contents/tp-progress-team.page-vue-render.js
@@ -734,6 +734,6 @@ with(this){return _c('td',[_c('div',[_c('p',[_c('a',{attrs:{"href":"https://gith
 with(this){return _c('td',[_c('div',[_c('img',{attrs:{"src":"https://github.com/AY2425S1-CS2103T-F14b-4/tp/workflows/Java%20CI/badge.svg"}})])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Tue, 22 Oct 2024, 11:33:32 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Tue, 22 Oct 2024, 11:58:54 GMT+8]")])])])}
 }];
   
